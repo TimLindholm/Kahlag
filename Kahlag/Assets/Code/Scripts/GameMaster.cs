4e3dae584df5f3de75b30879334cb2720003aaff
daniel <a@A.A>
2017-05-18 14:41:42 +0200

    Updated level and master script

--- Kahlag/Assets/Code/Scripts/GameMaster.cs
+++ Kahlag/Assets/Code/Scripts/GameMaster.cs
@@ -142,7 +142,7 @@ public class GameMaster : MonoBehaviour
     IEnumerator StartTheGame()
     {
         fadeScript.ShouldFading = false;
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(2f);
         TitleText.SetActive(false);
         gameCamera.position = gameplayPosition.position;
         gameCamera.rotation = gameplayPosition.rotation;
